8fc6c79e9753494c17f48dbad807a0cff29b3b75
Edwin Corzo <edwin11ramirez@gmail.com>
2015-04-20 13:30:44 -0700

    did some code

---
+++ html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="dan">
+		<p>
+			I like to play soccer
+		</p>
+	</div class="iwao">
+
+	<div>
+		<p>
+			My name is edwin
+		</p>
+	</div>
+	<script type="http://code.jquery.com/jquery-latest.min.js"></script>
+
+</body>
+</html>			
